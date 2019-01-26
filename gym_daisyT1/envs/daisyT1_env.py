ff15a18aff3824a87e628bf8513fac02d47c9e0d
GurmeharS <gurmehar.sandhu@mail.utoronto.ca>
2019-01-26 16:25:21 -0500

    Implemented environment class functions. Also added csv file to directory.

--- gym_daisyT1/envs/daisyT1_env.py
+++ gym_daisyT1/envs/daisyT1_env.py
@@ -13,33 +13,62 @@ class DaisyT1Env(gym.Env):
   def __init__(self):
     self.__version__ = "0.1.0"
 
-    #import dataframe of radii
+    # import dataframe of radii
     self.data = pd.read_csv("/csv/track_1.csv")
 
-    #General variables
-    self.MAX_VEL = 20
+    # General variables
+    self.MAX_VEL = 30
     self.MAX_ACC = 20
     self.H = 15
+    self.gas = 1000
+    self.tireDur = 1000
 
+    self.TOTAL_STEPS = len(self.data.radius)
 
-    for i in len(self.data.radius):
+    for i in range(0,len(self.data.radius)):
       if self.data.radius[i] == -1:
         self.MAX_VEL_ARR[i] = self.MAX_VEL
       else:
         self.MAX_VEL_ARR[i] = np.sqrt((self.data.radius[i]) * self.H / 1000000)
+
     self.currStep = -1
-    #Action space (what can the agent control)
+
+    # Action space (what can the agent control)
     self.action_space = np.linspace(-1*self.MAX_ACC,self.MAX_ACC,2*self.MAX_ACC + 1)
 
-    #Observation space (what can the agent see)
+    # Observation space (what can the agent see)
     self.observation_space = self.data
 
+    # Store what agent tried
+    self.curr_ep = -1
+    self.memory = []
 
   def step(self, action):
-    self.currStep+=1
-
+    self.currStep += 1
 
   def reset(self):
-    ...
+    """
+    Rset environment state
+    :return:
+    observation object: initial observation of the space
+
+    """
+    self.currStep = -1
+    self.curr_ep += 1
+    self.memory.append([])
+    self.gas = 1000
+
+    return self._get_state()
+
+  def _get_state(self):
+    """Return the Observation."""
+    return [self.TOTAL_STEPS - self.curr_step]
+
   def render(self, mode='human', close=False):
-    ...
+    return
+
+  def _get_reward(self):
+    if self.pitStop:
+      return (1/self.dt - 2)        #Defined reward
+    else:
+      return (1/self.dt)
