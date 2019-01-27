ca0bc9bd3a302f3d52d579d53b76b86be0c9df39
GurmeharS <gurmehar.sandhu@mail.utoronto.ca>
2019-01-26 22:18:55 -0500

    Need to train

--- gym_daisyT1/envs/daisyT1_env.py
+++ gym_daisyT1/envs/daisyT1_env.py
@@ -9,6 +9,8 @@ from gym import spaces
 from gym import error, spaces, utils
 from gym.utils import seeding
 
+run = 0
+
 class DaisyT1Env(gym.Env):
   metadata = {'render.modes': ['human']}
 
@@ -31,6 +33,7 @@ class DaisyT1Env(gym.Env):
     self.overMaxVel = False
     self.dt = 0
     self.firstDT = True
+    self.pCount = 0
 
     self.TOTAL_STEPS = len(self.data.radius)
     self.MAX_VEL_ARR = np.zeros((len(self.data), 1))
@@ -43,7 +46,7 @@ class DaisyT1Env(gym.Env):
         else:
             self.MAX_VEL_ARR[i] = (np.sqrt((self.data.radius[i]) * self.H / 1000000))
 
-    self.curr_step = 1
+    self.curr_step = -1
 
     # Action space (what can the agent control)
     self.action_space = np.array([])
@@ -59,41 +62,64 @@ class DaisyT1Env(gym.Env):
 
   def step(self, action):
     action -= self.MAX_ACC
-    self.currStep += 1
+    self.curr_step += 1
     self.takeAction(action)
     curr_reward = self._get_reward()
     ob = self._get_state()
+
     return ob, curr_reward, self.finishedState, {}
 
   def takeAction(self,action):
     
     if(action>0):
       self.gas -= 0.1*(action*action)
+      #print(self.gas)
     elif(action<0):
       self.tireDur -= 0.1*(action*action)
-    
-    if(self._get_state==0):
+
+    if(self._get_state==self.TOTAL_STEPS-1):
       self.finishedState=True
 
     if(self.pitStop):
       action = self.MAX_ACC
+      self.pitStop = False
+      self.pCount += 1
 
-    if(self.gas==0 or self.tireDur==0):
+
+    if(self.gas<=15 or self.tireDur<=15):
       action = self.takePitStop()
 
-    if((action + self.currVel)>self.MAX_VEL_ARR[self.currStep]):
-      self.currVel = self.MAX_VEL_ARR[self.currStep]
+    if((action + self.currVel)>self.MAX_VEL_ARR[self.curr_step]):
+      self.currVel = self.MAX_VEL_ARR[self.curr_step]
       self.overMaxVel = True
 
     if(action==0):
       if (self.currVel == 0):
         self.dt = 0
       else:
-        self.dt += 1/self.currVel
+        if(self.pitStop):
+          self.dt += 1/self.currVel
+        else:
+          self.dt += 1/self.currVel
     else:
-      self.nextVel = np.sqrt(abs(self.currVel*self.currVel + 2*action*1))
-      self.dt += (self.currVel + self.nextVel)/action
+      self.nextVel = np.sqrt(abs(self.currVel * self.currVel + 2 * action * 1))
+      if(self.pitStop):
+        self.dt += (self.currVel + self.nextVel)/action
+      else:
+        self.dt = (self.currVel + self.nextVel)/action
 
+    global run
+    run += 1
+    """
+    df = pd.DataFrame([[run, action]], columns=["Iteration", "Acceleration"])
+    if run == 1:
+        df.to_csv("output.csv")
+    if run != 1:
+        df.to_csv("output.csv", header=None, mode="a")
+    print(action)
+    """
+    self.nextVel = self.currVel
+    self.memory += [action]
     return True
 
   def takePitStop(self):
@@ -109,30 +135,46 @@ class DaisyT1Env(gym.Env):
     :return:
     observation object: initial observation of the space
     """
-    self.currStep = -1
-    self.curr_ep += 1
-    self.memory.append([])
+
+    self.MAX_VEL = 30
+    self.MAX_ACC = 20
+    self.H = 15
     self.gas = 1000
+    self.tireDur = 1000
+    self.pitStop = False
+    self.currVel = 0
+    self.nextVel = 0
+    self.finishedState = False
+    self.overMaxVel = False
+    self.dt = 0
+    self.firstDT = True
+    self.pCount = 0
+    self.memory = []
+    #print(self.memory)
+    self.curr_step = -1
+    self.curr_ep += 1
+    #self.memory.append([])
 
     return self._get_state()
 
   def _get_state(self):
     """Return the Observation."""
-    return [self.TOTAL_STEPS - self.curr_step]
+    return self.curr_step
 
   def render(self, mode='human', close=False):
     return
 
   def _get_reward(self):
+    #print(self.dt)
     if(self.pitStop):
-      self.pitStop = False
-      return (1/self.dt - 2)        #Defined reward
+      #self.pitStop = False
+      return (-1)        #Defined reward
     elif(self.overMaxVel):          #Action takes us over the maximum velocity for the curve
       self.overMaxVel = False
-      return (1/self.dt - 1)
+      return (100/self.dt - 1)
     else:
       if (self.currVel == 0):
         self.firstDT = False
         return -1
-      return (1/self.dt)
+      return (100/self.dt)
 
--- qlearn.py
+++ qlearn.py
@@ -1,9 +1,10 @@
 #!pip -q install gym
 import numpy as np
+import pandas as pd
 import gym
 import gym_daisyT1
 import random
-
+LastEpisode = False
 env=gym.make('daisyT1-v0')
 env.render()
 
@@ -15,9 +16,9 @@ print("State size ",state_size)
 qtable = np.zeros((state_size,action_size))
 print(qtable)
 
-total_episodes = 10 #total episodes
-total_test_episodes = 100 #total test episodes
-max_steps = 99 #max steps per episode
+total_episodes = 2000 #total episodes
+total_test_episodes = 1 #total test episodes
+max_steps = env.TOTAL_STEPS #max steps per episode
 
 learning_rate = 0.7 #learning rate
 gamma = 0.618 #discount rate
@@ -25,16 +26,15 @@ gamma = 0.618 #discount rate
 epsilon = 1.0 #exploration rate
 max_epsilon = 1.0 #max expl. probability at start
 min_epsilon = 0.01 #min expl. probability 
-decay_rate = 0.01 #exp. decay for expl.
+decay_rate = 0.18 #exp. decay for expl.
 
 for episode in range(total_episodes):
     state = env.reset()
     step=0
     done=False
-    print("line 34")
     for step in range(env.TOTAL_STEPS):
         exp_exp_tradeoff = random.uniform(0,1)
-
+        #print(exp_exp_tradeoff)
         #exploitation
         if exp_exp_tradeoff > epsilon:
             action = np.argmax(qtable[state,:])
@@ -42,23 +42,27 @@ for episode in range(total_episodes):
         #random choice - exploration
         else:
             action = env.action_space.sample()
-        
         #take action and record state s' and reward
+
         new_state, reward, done, info = env.step(action)
-        
+        #action >0 here, <40
+        #if abs(action) > 20: print("wow")
         #Update Q(s,a):= Q(s,a)+
         qtable[state,action] = qtable[state,action] + learning_rate*(reward + gamma*np.max(qtable[new_state,:]) - qtable[state,action])
-        
+
+        #print([state,action])
         state = new_state
         
         if done==True:
             break
     
     episode += 1
-    
+    #print(epsilon)
     #reduce epsilon to move towards exploitation
     epsilon = min_epsilon + (max_epsilon-min_epsilon)*np.exp(-decay_rate*episode)
-
+#print(env.memory)
+df = pd.DataFrame([env.memory], )
+#print(qtable)
 env.reset()
 rewards=[]
 
@@ -67,6 +71,7 @@ for episode in range(total_test_episodes):
     step=0
     done=False
     total_rewards = 0
+    finalActions = []
     print('*******************************')
     print('Episode is ', episode)
     
@@ -74,15 +79,19 @@ for episode in range(total_test_episodes):
         #env.render()
         #take action that gives maximum expected future reward given state
         action=np.argmax(qtable[state,:])
-        print(action)
-        new_state, reward, sdone, info = env.step(action)
+
+        #print(qtable[state,:])
+        #print(action)
+
+        new_state, reward, done, info = env.step(action)
         
-        total_rewards+= reward
-        
+        total_rewards += reward
+        finalActions += [np.argmax(qtable[state,:])-env.MAX_ACC]
         if done:
             rewards.append(total_rewards)
             print('Score ', total_rewards)
             break
         state=new_state
+print(finalActions)
 env.close()
 print('Score over time: ' + str(sum(rewards)/total_test_episodes))
